753697cc26a6c38c954796406e337688d0d5f26c
piyush-mit <piyushmit5758@gmail.com>
2026-01-17 19:33:33 +0530

    added build scripts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,22 @@
-import express from 'express' ;
+import express, { Request, Response } from 'express' ;
 import { ENV } from './lib/env.js';
-
+import path from 'path';
 const app = express();
-
+const __dirname = path.resolve();
 app.use(express.json())
 
+
+app.get("/health",(req : Request , res : Response ) => {
+    res.status(200).json({message : "hey welcome to meetcode"});
+})
+
+if(ENV.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname , '../frontend/dist')));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname , "../frontend", "dist" , "index.html"));
+    })
+}
 app.listen(ENV.PORT , () => {
     console.log("Server active on port" , ENV.PORT);
 })
